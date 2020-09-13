c8ccb1c973f2f389002a589635a027d10a965486
zhangli2946 <zhangli2946@gmail.com>
2020-09-13 16:19:09 +0800

    accept other env as is

--- configuration.go
+++ configuration.go
@@ -1,14 +1,14 @@
 package configuration
 
 import (
-	"os"
-	"strings"
-	"sync"
+    "os"
+    "strings"
+    "sync"
 )
 
 const (
-	Prefix = "SERVICE"
-	Sep    = "="
+    Prefix = "SERVICE"
+    Sep    = "="
 )
 
 type Configuration = map[string]interface{}
@@ -16,29 +16,31 @@ type Configuration = map[string]interface{}
 type Modifier func(Configuration) error
 
 var (
-	mutable Configuration
-	once    = new(sync.Once)
+    mutable Configuration
+    once    = new(sync.Once)
 )
 
 func fromEnv() {
-	mutable = Configuration{}
-	for _, eq := range os.Environ() {
-		tmp := strings.SplitN(eq, Sep, 2)
-		if len(tmp) != 2 {
-			continue
-		}
-		if strings.HasPrefix(tmp[0], Prefix) {
-            mutable[strings.ReplaceAll(strings.ToLower(tmp[0]),"_",".")] = tmp[1]
-		}
-	}
+    mutable = Configuration{}
+    for _, eq := range os.Environ() {
+        tmp := strings.SplitN(eq, Sep, 2)
+        if len(tmp) != 2 {
+            continue
+        }
+        if strings.HasPrefix(tmp[0], Prefix) {
+            mutable[strings.ReplaceAll(strings.ToLower(tmp[0]), "_", ".")] = tmp[1]
+        } else {
+            mutable[tmp[0]] = tmp[1]
+        }
+    }
 }
 
 func init() {
-	once.Do(func() {
-		fromEnv()
-	})
+    once.Do(func() {
+        fromEnv()
+    })
 }
 
 func FromEnv() Configuration {
-	return mutable
+    return mutable
 }
